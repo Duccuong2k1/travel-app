c9e9b07787a56dfed51fd37095d942cac400deea
duc cuong <doduccuong2001@gmail.com>
2021-08-25 16:39:55 +0700

    commit app yoot task

---
+++ src/redux/rootReducer/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import demoSlice from '../slice/appSlice/demoSlice'
+export const rootReducer = combineReducers({
+    demoSlice
+});
+export type RootState = ReturnType<typeof rootReducer>;
